ebdad622f7905093f7e54594a2de918933ea3d0e
anicolson <aaron.nicolson@csiro.au>
2023-06-14 06:57:27 +0800

    Update.

--- tools/copy_ckpt_bowen_to_setonix.sh
+++ tools/copy_ckpt_bowen_to_setonix.sh
@@ -1,2 +1,15 @@
-scp /datasets/work/hb-mlaifsp-mm/work/repositories/transmodal/multimodal_23/task/mimic_cxr/results/miccai_2023_any_lora.ipynb \
-    anicolson@data-mover.pawsey.org.au:/software/projects/pawsey0864/anicolson/repositories/cxrmate/
+# cxrmate-single-tf:
+scp /datasets/work/hb-mlaifsp-mm/work/experiments/mimic_cxr/082_any_single/trial_1/epoch=11-val_report_chexbert_f1_macro=0.347756.ckpt \
+    anicolson@data-mover.pawsey.org.au:/scratch/pawsey0864/anicolson/experiments/mimic_cxr/082_any_single/trial_1/
+
+# cxrmate-variable-tf:
+scp /datasets/work/hb-mlaifsp-mm/work/experiments/mimic_cxr/083_any_variable/trial_1/epoch=28-val_report_chexbert_f1_macro=0.383505.ckpt \
+    anicolson@data-mover.pawsey.org.au:/scratch/pawsey0864/anicolson/experiments/mimic_cxr/083_any_variable/trial_1/
+
+# cxrmate-tf:
+scp /datasets/work/hb-mlaifsp-mm/work/experiments/mimic_cxr/091_any_prompt_variable_lora/trial_0/epoch=6-step=27433-val_report_chexbert_f1_macro=0.388249.ckpt \
+    anicolson@data-mover.pawsey.org.au:/scratch/pawsey0864/anicolson/experiments/mimic_cxr/091_any_prompt_variable_lora/trial_0/
+
+# cxrmate:
+scp /datasets/work/hb-mlaifsp-mm/work/experiments/mimic_cxr/098_gen_prompt_cxr_bert/trial_0/epoch=0-step=3917-val_report_chexbert_f1_macro=0.425015.ckpt \
+    anicolson@data-mover.pawsey.org.au:/scratch/pawsey0864/anicolson/experiments/mimic_cxr/098_gen_prompt_cxr_bert/trial_0/
